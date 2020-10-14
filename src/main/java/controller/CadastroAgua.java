3e470953f142a26ead65142f73e133bd6eb00a01
GabrielSG20 <gabrielsoaresg20@gmail.com>
2020-10-14 22:50:13 +0000

    Mensagem de sucesso no Cadastro

--- src/main/java/controller/CadastroAgua.java
+++ src/main/java/controller/CadastroAgua.java
@@ -6,8 +6,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-
+import javafx.scene.control.Alert.AlertType;
 import application.Main;
 
 public class CadastroAgua implements Initializable {
@@ -29,6 +30,12 @@ public class CadastroAgua implements Initializable {
     
     public void changeScreenVoltarTelaInicial(ActionEvent event){
         Main.changeScreen("main");
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Confirmação de Cadastro");
+        alert.setHeaderText(null);
+        alert.setContentText("Cadastro realizado com sucesso!");
+
+        alert.showAndWait();
     }
     
     public void salvarCadastro(ActionEvent evenent){
