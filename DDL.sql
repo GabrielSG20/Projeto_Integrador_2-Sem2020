535206f3bce308fab441a598e6fba285a82605e3
ferrazghs <bi.henriquesf@hotmail.com>
2020-10-26 22:22:14 +0000

    tabela conta de energia

--- DDL.sql
+++ DDL.sql
@@ -40,9 +40,31 @@ create table cta_conta (
     cta_vencimento date not null,
     constraint cta_conta_pk primary key (int_numero_instalacao,cta_mes_referencia)
 );
-      
+
+create table ene_energia (
+    int_numero_instalacao bigint not null,
+    cta_mes_referencia date not null,
+    ene_consumo_conta_mes bigint not null,
+    ene_codigo_fiscal bigint not null,
+    ene_grupo_subgrupo bigint not null,
+    ene_tipo_fornecimento varchar(50) not null,
+    ene_classe_subclasse varchar(50) not null,
+    ene_roteiro_leitura varchar(50) not null,
+    ene_modalidade_tarifaria varchar(30) not null,
+    ene_tensao_nominal varchar(20) not null,
+    ene_numero_medidor bigint not null,
+    ene_const_multi numeric(10,2) not null,
+    ene_leitura_anterior_cod bigint not null,
+    ene_leitura_atual_cod bigint not null,
+    ene_data_leitura_anterior date not null,
+    ene_data_leitura_atual date not null,
+    ene_tipo_bandeira varchar(10) not null,
+    ene_valor_total numeric (10,2) not null
+);
+
 alter table cta_conta add constraint instalacao_conta_fk foreign key (int_numero_instalacao) references int_instalacao(int_numero_instalacao);
 alter table int_instalacao add constraint cliente_instalacao_fk foreign key (cli_documento) references cli_cliente(cli_documento);
 alter table int_instalacao add constraint fornecedor_instalacao_fk foreign key (for_cnpj) references for_fornecedor(for_cnpj);
 alter table int_instalacao add constraint endereco_instalacao_fk foreign key (end_cep, end_numero) references end_endereco(end_cep,end_numero);
+alter table ene_energia add constraint conta_conta_energia_fk foreign key (int_numero_instalacao, cta_mes_referencia) references cta_conta(int_numero_instalacao, cta_mes_referencia);
 
