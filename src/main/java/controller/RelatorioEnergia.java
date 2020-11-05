402bd6647df3db88b36a569e8fe45d3077c183d5
ferrazghs <bi.henriquesf@hotmail.com>
2020-11-05 00:40:27 +0000

    Alterações DAO e Table

--- src/main/java/controller/RelatorioEnergia.java
+++ src/main/java/controller/RelatorioEnergia.java
@@ -1,19 +1,35 @@
 package controller;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import java.math.BigInteger;
+import java.net.URL;
+import java.util.ResourceBundle;
 import classes.Energia;
-public class controller {
+import dao.EnergiaDAO;
+
+public class RelatorioEnergia implements Initializable {
     @FXML
-    private TableView<Energia> TabelaEnergia;
+    private TableView TabelaEnergia;
     @FXML
-    private TableColumn NumInstalacao;
+    private TableColumn <Energia, BigInteger> NumInstalacao;
     @FXML
-    private TableColumn DocCliente;
+    private TableColumn <Energia, BigInteger> DocCliente;
     @FXML
-    private TableColumn CNPJFornecedor;
+    private TableColumn <Energia, BigInteger> CNPJFornecedor;
     @FXML
-    private TableColumn CEP;
+    private TableColumn <Energia, BigInteger> CEP;
     @FXML
-    private TableColumn NResidencia;
+    private TableColumn <Energia, BigInteger> NResidencia;
+    
+    @Override
+     
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+
+    
+
 }
