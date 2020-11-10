33983d9fc83f8a7b8d1776f697958e87fbaf362e
GabrielSG20 <gabrielsoaresg20@gmail.com>
2020-11-10 22:43:33 +0000

    Senha BD alterada

--- src/main/java/conexao/ConexaoBd.java
+++ src/main/java/conexao/ConexaoBd.java
@@ -13,7 +13,7 @@ public class ConexaoBd {
     private static final String DRIVER = "com.mysql.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/cadastro_contas";
     private static final String USER = "root";
-    private static final String PASS = "";
+    private static final String PASS = "admin";
     
     // Metodo para fazer a conexao junto a base de dados mysql.
     public static Connection getConnection() {
